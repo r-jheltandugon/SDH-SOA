90cd25711b2229f10f68d7361ede03371e6b360a
R-jhel B. Tandugon <jherietandugon@gmail.com>
2025-05-09 18:03:37 +0800

    Add values

--- soa_app/print.php
+++ soa_app/print.php
@@ -13,6 +13,56 @@ if (!$result || $result->num_rows === 0) {
 }
 
 $data = $result->fetch_assoc();
+
+$drugs_discount = $data['drugs_and_meds'] * 0.20; 
+$labs_discount = $data['lab_and_diag'] * 0.20; 
+$misc_discount = $data['misc'] * 0.20;
+$room_discount = $data['room_and_board'] * 0.20;
+$other_discount = $data['other'] * 0.20;
+$service_discount = $data['service'] * 0.20;
+$supplies_discount = $data['supplies'] * 0.20;
+$discount = $drugs_discount + $labs_discount + $misc_discount + $room_discount + $other_discount + $service_discount + $supplies_discount;
+
+$total_drugs_and_meds = $data['drugs_and_meds'] - $drugs_discount;
+$total_lab_and_diag = $data['lab_and_diag'] - $labs_discount;
+$total_misc = $data['misc'] - $misc_discount;
+$total_room_and_board = $data['room_and_board'] - $room_discount;
+$total_other = $data['other'] - $other_discount;
+$total_service = $data['service'] - $service_discount;
+$total_supplies = $data['supplies'] - $supplies_discount;
+
+$case_rate_1a = $data['case_rate_1a'] ?? 0;
+$case_rate_1b = $data['case_rate_1b'] ?? 0;
+
+$subtotal_hci_fees = $data['total_hci_fees'] - $discount - $case_rate_1a;
+
+$doctor_fees_result = $mysqli->query("SELECT * FROM doctor_fees WHERE statement_id = $id");
+
+$doctor_fees = [];
+if ($doctor_fees_result && $doctor_fees_result->num_rows > 0) {
+    while ($row = $doctor_fees_result->fetch_assoc()) {
+        $doctor_fees[] = $row;
+    }
+}
+
+$total_doctor_fee = 0;
+$doctor_fee_discount = 0;
+
+foreach ($doctor_fees as $doc) {
+    $total_doctor_fee += $doc['doctor_fee'];
+    $doctor_fee_discount += $doc['doctor_fee'] * 0.20;
+}
+
+$pfsubtotal = max(0, $total_doctor_fee - $doctor_fee_discount - $case_rate_1b);
+
+$total_discount = $discount + $doctor_fee_discount;
+
+$total_case_1 = $case_rate_1a + $case_rate_1b;
+
+$total_actual_charges = $data['total_hci_fees'] + $total_doctor_fee;
+
+$total_doh = $total_actual_charges - $total_discount - $total_case_1;
+
 ?>
 
 <!DOCTYPE html>
@@ -147,7 +197,7 @@ th, td {
     SALCEDO DOCTORS HOSPITAL, INC.<br>
     Salcedo, Eastern Samar<br>
     Call No. 09224458879<br>
-    <span style="color:rgb(255, 0, 157); font-style: italic;"">At SDH.. you are in the PINK of Health</span>
+    <span style="color:rgb(255, 0, 157); font-style: italic;">At SDH.. you are in the PINK of Health</span>
 </div>
 
 <table class="no-border">
@@ -160,7 +210,7 @@ th, td {
 
         <td style="text-align: center; width: 20%;"><strong>Age:</strong> 
             <span style="display: inline-block; min-width: 30px; border-bottom: 1px solid #000;">
-                <?= htmlspecialchars($data['age'] ?? '-') ?>
+                <?= htmlspecialchars($data['p_age'] ?? '-') ?>
             </span>
         </td>
 
@@ -186,11 +236,11 @@ th, td {
     </tr>
     <tr>
         <td colspan="2"><strong>Final Diagnosis:</strong> <span style="display: inline-block; min-width: 250px; border-bottom: 1px solid #000;"><?= nl2br(htmlspecialchars($data['diagnosis'])) ?></span></td>
-        <td class="right-align" colspan="2"><strong>First Case Rate:</strong> <span style="display: inline-block; min-width: 100px; border-bottom: 1px solid #000;"></span></td>
+        <td class="right-align" colspan="2"><strong>First Case Rate:</strong> <span style="display: inline-block; min-width: 100px; border-bottom: 1px solid #000; text-align: left;"><?= nl2br(htmlspecialchars($data['first_case_rate'])) ?></span></td>
     </tr>
     <tr>
-        <td colspan="2"><strong>Other Diagnosis:</strong> <span style="display: inline-block; min-width: 250px; border-bottom: 1px solid #000;"><?= nl2br(htmlspecialchars($data['other_diagnosis'] ?? '-')) ?></span></td> 
-        <td class="right-align" colspan="2"><strong>Second Case Rate:</strong> <span style="display: inline-block; min-width: 100px; border-bottom: 1px solid #000;"></span></td>
+        <td colspan="2"><strong>Other Diagnosis:</strong> <span style="display: inline-block; min-width: 250px; border-bottom: 1px solid #000;"><?= nl2br(htmlspecialchars($data['other_diagnosis'])) ?></span></td> 
+        <td class="right-align" colspan="2"><strong>Second Case Rate:</strong> <span style="display: inline-block; min-width: 100px; border-bottom: 1px solid #000; text-align: left;"><?= nl2br(htmlspecialchars($data['second_case_rate'])) ?></span></td>
     </tr>
 </table>
 
@@ -198,7 +248,7 @@ th, td {
 
 <table class="summary">
     <tr>
-        <th rowspan="7">Particular</th>
+        <th colspan="2" rowspan="7">Particular</th>
         <th rowspan="7" class="center">Actual Charges</th>
         <th colspan="5" class="center">Amount of Discount</th>
         <th colspan="2" class="center">PhilHealth Benefits</th>
@@ -214,11 +264,11 @@ th, td {
     </tr>
     <tr>
         <td></td>
-        <td class="left" colspan="2">PCSO</td>
+        <td class="left" colspan="3">PCSO</td>
     </tr>
     <tr>
         <td></td>
-        <td class="left" colspan="2">DSWD</td>
+        <td class="left" colspan="3">DSWD</td>
     </tr>
     <tr>
         <td class="center">✔</td>
@@ -233,110 +283,130 @@ th, td {
         <td class="left" colspan="2">Others:</td>
     </tr>
     <tr>
-        <td><strong>Drugs and Medicines</strong></td>
-        <td class="center">₱ <?= number_format($data['services'] ?? 0, 2) ?></td>
-        <td class="center"></td>
-        <td class="center"></td>
-        <td class="center" colspan="3"></td>
-        <td class="center"></td>
-        <td class="center"></td>
-        <td class="center"></td>
+        <td colspan="2" style="background-color:rgb(235, 235, 235);"><strong>Drugs and Medicines</strong></td>
+        <td><strong>₱ <span style="margin-left: 20px;"><?= number_format($data['drugs_and_meds'] ?? 0, 2) ?></span></strong></td>
+        <td ></td>
+        <td><strong>₱ <span style="margin-left: 20px;"><?= number_format($drugs_discount, 2) ?></span></strong></td>
+        <td colspan="3"><strong>₱ <span style="margin-left: 20px;"><?= number_format($total_drugs_and_meds, 2) ?></span></strong></td> 
+        <td ></td>
+        <td ></td>
+        <td ></td>
     </tr>
     <tr>
-        <td><strong>Laboratory and Diagnostics</strong></td>
-        <td class="center">₱ <?= number_format($data['supplies'] ?? 0, 2) ?></td>
-        <td class="center"></td>
-        <td class="center"></td>
-        <td class="center" colspan="3"></td>
-        <td class="center"></td>
-        <td class="center"></td>
-        <td class="center"></td>
+        <td  colspan="2" style="background-color:rgb(235, 235, 235);"><strong>Laboratory and Diagnostics</strong></td>
+        <td>₱ <span style="margin-left: 20px;"><?= number_format($data['lab_and_diag'] ?? 0, 2) ?></span></td>
+        <td ></td>
+        <td><strong>₱ <span style="margin-left: 20px;"><?= number_format($labs_discount, 2) ?></span></strong></td>
+        <td colspan="3"><strong>₱ <span style="margin-left: 20px;"><?= number_format($total_lab_and_diag, 2) ?></span></strong></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
     </tr>
     <tr>
-        <td><strong>Miscellaneous</strong></td>
-        <td class="center">₱ <?= number_format($data['supplies'] ?? 0, 2) ?></td>
-        <td class="center"></td>
-        <td class="center"></td>
-        <td class="center" colspan="3"></td>
-        <td class="center"></td>
-        <td class="center"></td>
-        <td class="center"></td>
+        <td colspan="2" style="background-color:rgb(235, 235, 235);"><strong>Miscellaneous</strong></td>
+        <td>₱ <span style="margin-left: 20px;"><?= number_format($data['misc'] ?? 0, 2) ?></span></td>
+        <td ></td>
+        <td><strong>₱ <span style="margin-left: 20px;"><?= number_format($misc_discount, 2) ?></span></strong></td>
+        <td colspan="3"><strong>₱ <span style="margin-left: 20px;"><?= number_format($total_misc, 2) ?></span></strong></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
     </tr>
     <tr>
-        <td><strong>Room and Board</strong></td>
-        <td class="center">₱ <?= number_format($data['services'] ?? 0, 2) ?></td>
-        <td class="center"></td>
-        <td class="center"></td>
-        <td class="center" colspan="3"></td>
-        <td class="center"></td>
-        <td class="center"></td>
-        <td class="center"></td>
+        <td colspan="2" style="background-color:rgb(235, 235, 235);"><strong>Room and Board</strong></td>
+        <td>₱ <span style="margin-left: 20px;"><?= number_format($data['room_and_board'] ?? 0, 2) ?></span></td>
+        <td ></td>
+        <td><strong>₱ <span style="margin-left: 20px;"><?= number_format($room_discount, 2) ?></span></strong></td>
+        <td colspan="3"><strong>₱ <span style="margin-left: 20px;"><?= number_format($total_room_and_board, 2) ?></span></strong></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
     </tr>
     <tr>
-        <td><strong>Supplies</strong></td>
-        <td class="center">₱ <?= number_format($data['supplies'] ?? 0, 2) ?></td>
-        <td class="center"></td>
-        <td class="center"></td>
-        <td class="center" colspan="3"></td>
-        <td class="center"></td>
-        <td class="center"></td>
-        <td class="center"></td>
+        <td colspan="2" style="background-color:rgb(235, 235, 235);"><strong>Other</strong></td>
+        <td>₱ <span style="margin-left: 20px;"><?= number_format($data['other'] ?? 0, 2) ?></span></td>
+        <td ></td>
+        <td><strong>₱ <span style="margin-left: 20px;"><?= number_format($other_discount, 2) ?></span></strong></td>
+        <td colspan="3"><strong>₱ <span style="margin-left: 20px;"><?= number_format($total_other, 2) ?></span></strong></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
     </tr>
     <tr>
-        <td><strong>HCI Subtotal</strong></td>
-        <td class="center"><strong style="font-size: 14px">₱ 0.00</strong></td>
-        <td class="center"></td>
-        <td class="center"><strong style="font-size: 14px">₱ 0.00</strong></td>
-        <td class="center" colspan="3"><strong style="font-size: 14px">₱ 0.00</strong></td>
-        <td class="center"><strong>₱ 0.00</strong></td>
-        <td class="center"><strong>₱ 0.00</strong></td>
-        <td class="center"></td>
+        <td colspan="2" style="background-color:rgb(235, 235, 235);"><strong>Service</strong></td>
+        <td>₱ <span style="margin-left: 20px;"><?= number_format($data['service'] ?? 0, 2) ?></span></td>
+        <td ></td>
+        <td><strong>₱ <span style="margin-left: 20px;"><?= number_format($service_discount, 2) ?></span></strong></td>
+        <td colspan="3"><strong>₱ <span style="margin-left: 20px;"><?= number_format($total_service, 2) ?></span></strong></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
     </tr>
     <tr>
-        <td colspan="10"><strong>Professional Fee/s</strong></td>
+        <td colspan="2" style="background-color:rgb(235, 235, 235);"><strong>Supplies</strong></td>
+        <td>₱ <span style="margin-left: 20px;"><?= number_format($data['supplies'] ?? 0, 2) ?></span></td>
+        <td ></td>
+        <td><strong>₱ <span style="margin-left: 20px;"><?= number_format($supplies_discount, 2) ?></span></strong></td>
+        <td colspan="3"><strong>₱ <span style="margin-left: 20px;"><?= number_format($total_supplies, 2) ?></span></strong></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
     </tr>
     <tr>
-        <td><strong>Professional Fee/s</strong></td>
-        <td class="center"><strong>₱ 0.00</strong></td>
-        <td class="center"></td>
-        <td class="center"></td>
-        <td class="center" colspan="3"></td>
-        <td class="center"></td>
-        <td class="center"></td>
-        <td class="center"></td>
+        <td colspan="2"><strong>HCI Subtotal</strong></td>
+        <td><strong style="font-size: 14px;">₱ <span style="margin-left: 10px;"><?= number_format($data['total_hci_fees'] ?? 0, 2) ?></span></strong></td>
+        <td ></td>
+        <td><strong style="font-size: 14px;">₱ <span style="margin-left: 10px;"><?= number_format($discount, 2) ?></span></strong></td>
+        <td colspan="3"><strong style="font-size: 14px;">₱ <span style="margin-left: 10px;"><?= number_format($subtotal_hci_fees, 2) ?></span></strong></td>
+        <td style="vertical-align: middle;"><strong>₱ <span style="margin-left: 10px;"><?= number_format($data['case_rate_1a'] ?? 0, 2) ?></span></strong></td>
+        <td style="vertical-align: middle;"><strong>₱ 0.00</strong></td>
+        <td ></td>
     </tr>
     <tr>
-        <td><strong>PF Subtotal</strong></td>
-        <td class="center"><strong style="font-size: 14px">₱ 0.00</strong></td>
-        <td class="center"></td>
-        <td class="center"><strong style="font-size: 14px">₱ 0.00</strong></td>
-        <td class="center" colspan="3"><strong>₱ 0.00</strong></td>
-        <td class="center"><strong>₱ 0.00</strong></td>
-        <td class="center"><strong>₱ 0.00</strong></td>
-        <td class="center"></td>
+        <td colspan="11" ><strong>Professional Fee/s</strong></td>
+    </tr>
+    <tr><?php foreach ($doctor_fees as $doc): ?>
+        <td colspan="2" style="background-color:rgb(235, 235, 235);"><strong><?= htmlspecialchars($doc['doctor_name']) ?></strong></td>
+        <td ><strong>₱ <?= htmlspecialchars($doc['doctor_fee']) ?></strong></td>
+        <td ></td>
+        <td style="vertical-align: middle;"><strong>₱ <?= number_format($doctor_fee_discount, 2) ?></strong></td>
+        <td  colspan="3"></td>
+        <td ></td>
+        <td ></td>
+        <td></td>
+    </tr><?php endforeach; ?>
+    <tr>
+        <td colspan="2"><strong>PF Subtotal</strong></td>
+        <td ><strong style="font-size: 14px">₱ <?= htmlspecialchars($doc['doctor_fee']) ?></strong></td>
+        <td ></td>
+        <td ><strong style="font-size: 14px">₱ <?= number_format($doctor_fee_discount, 2) ?></strong></td>
+        <td style="vertical-align: middle;" colspan="3"><strong>₱ <?= number_format($pfsubtotal, 2) ?></strong></td>
+        <td style="vertical-align: middle;"><strong>₱ <span style="margin-left: 10px;"><?= number_format($data['case_rate_1b'] ?? 0, 2) ?></span></strong></td>
+        <td style="vertical-align: middle;"><strong>₱ 0.00</strong></td>
+        <td ></td>
     </tr>
     <tr>
-        <td><strong>Totals</strong></td>
-        <td class="center"><strong style="font-size: 14px">₱ 0.00</strong></td>
-        <td class="center"></td>
-        <td class="center"><strong style="font-size: 14px">₱ 0.00</strong></td>
-        <td class="center" colspan="3"><strong>₱ 0.00</strong></td>
-        <td class="center"><strong>₱ 0.00</strong></td>
-        <td class="center"><strong>₱ 0.00</strong></td>
-        <td class="center"></td>
+        <td colspan="2"><strong>Totals</strong></td>
+        <td><strong style="font-size: 14px">₱ <?= number_format($total_actual_charges, 2) ?></strong></td>
+        <td ></td>
+        <td ><strong style="font-size: 14px">₱ <?= number_format($total_discount, 2) ?></strong></td>
+        <td style="vertical-align: middle;" colspan="3"><strong>₱ <?= number_format($total_doh, 2) ?></strong></td>
+        <td style="vertical-align: middle;"><strong>₱ <span style="margin-left: 10px;"><?= number_format($total_case_1, 2) ?></span></strong></td>
+        <td style="vertical-align: middle;"><strong>₱ 0.00</strong></td>
+        <td ></td>
     </tr>
     <tr>
-        <td><strong>DOH MAIP</strong></td>
-        <td class="center"></td>
-        <td class="center"></td>
-        <td class="center"></td>
-        <td class="center" colspan="3"><strong style="font-size: 14px">₱ 0.00</strong></td>
-        <td class="center"></td>
-        <td class="center"></td>
-        <td class="center"></td>
+        <td colspan="2"><strong>DOH MAIP</strong></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
+        <td colspan="3"><strong style="font-size: 14px">₱ <?= number_format($total_doh, 2) ?></strong></td>
+        <td ></td>
+        <td ></td>
+        <td ></td>
     </tr>
     <tr>
-        <td colspan="9"><strong>Total Payable</strong></td>
+        <td colspan="10"><strong>Total Payable</strong></td>
         <td><strong style="font-size: 14px">₱ 0.00</strong></td>
     </tr>
 </table>
